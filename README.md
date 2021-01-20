a14aa46b6ec59bc4b89e2f140742fe9a2cb7101a
Sascha Greuel <sascha@softcreatr.de>
2021-01-20 23:47:05 +0100

    Updates

--- README.md
+++ README.md
@@ -5,5 +5,5 @@
 
 ![image](https://user-images.githubusercontent.com/81188/93883886-755c1980-fce2-11ea-824f-cb97a936290e.png)
 
-[Web visual](https://softcreatr.github.io/historical-weather-data-germany/)<br>
+[Web visual](https://1-2.dev/historical-weather-data-germany)<br>
 Favicon by [Just UI](https://www.iconfinder.com/icons/669958/cloud_forecast_sun_weather_icon)
--- docs/main.js
+++ docs/main.js
@@ -1,68 +1,85 @@
 'use strict';
 
-const xhr = new XMLHttpRequest();
-const weather = [];
+let weather = [];
 
-xhr.open('GET', 'assets/data.txt');
-xhr.send();
-xhr.onload = function () {
-    xhr.response
-        .split('\n')
-        .filter(d => d)
-        .splice(-30)
-        .forEach(day => {
-            const xhr2 = new XMLHttpRequest();
+const fetchDataList = async file => {
+  const response = await fetch('assets/data.txt');
 
-            xhr2.open('GET', `assets/data/${day}.json`);
-            xhr2.send();
-            xhr2.onload = function () {
-                weather.push({
-                    date: day,
-                    mmdd: day.substring(4, 6) + '/' + day.slice(-2),
-                    data: JSON.parse(xhr2.response)
-                });
-            };
-        });
-};
+  if (!response.ok) {
+    const message = `An error has occured: ${response.status}`;
+    throw new Error(message);
+  }
+
+  return await response.text();
+}
+
+const fetchDailyData = async day => {
+  const response = await fetch(`assets/data/${day}.json` );
+
+  if (!response.ok) {
+    const message = `An error has occured: ${response.status}`;
+    throw new Error(message);
+  }
+
+  return await response.json();
+}
+
+fetchDataList()
+  .then(dataList => {
+    dataList
+      .split('\n')
+      .filter(d => d)
+      .splice(-30)
+      .forEach(day => {
+        fetchDailyData(day)
+          .then(data => {
+            weather.push({
+              date: day,
+              mmdd: day.substring(4, 6) + '/' + day.slice(-2),
+              data: data
+            });
+          });
+      });
+  });
 
 setTimeout(() => {
-    const results = weather.sort((a, b) => a.date - b.date);
-    const labels = results.map(x => x.mmdd);
-    const ctx = document.getElementById('myChart').getContext('2d');
+  const results = weather.sort((a, b) => a.date - b.date);
+  const labels = results.map(x => x.mmdd);
+  const ctx = document.getElementById('myChart').getContext('2d');
 
-    new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: labels,
-            datasets: [
-                {
-                    label: 'Temp (\u{2103})',
-                    backgroundColor: 'rgba(255, 255, 255, .75)',
-                    borderColor: 'rgba(222, 222, 222, .75)',
-                    borderWidth: 1,
-                    data: results.map(x => Math.round(x.data.main.temp)),
-                    fill: false,
-                    cubicInterpolationMode: 'monotone'
-                },
-                {
-                    label: 'Min (\u{2103})',
-                    backgroundColor: 'rgba(52, 100, 189, .75)',
-                    borderColor: 'rgba(26, 100, 163, .75)',
-                    borderWidth: 1,
-                    data: results.map(x => Math.round(x.data.main.temp_min)),
-                    fill: false,
-                    cubicInterpolationMode: 'monotone'
-                },
-                {
-                    label: 'Max (\u{2103})',
-                    backgroundColor: 'rgba(189, 52, 52, .75)',
-                    borderColor: 'rgba(163, 26, 26, .75)',
-                    borderWidth: 1,
-                    data: results.map(x => Math.round(x.data.main.temp_max)),
-                    fill: false,
-                    cubicInterpolationMode: 'monotone'
-                },
-            ]
+  new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          label: 'Temp (\u{2103})',
+          backgroundColor: 'rgb(237, 78, 78)',
+          borderColor: 'rgba(255, 0, 0, .75)',
+          borderWidth: 1,
+          data: results.map(x => Math.round(x.data.main.temp)),
+          fill: false,
+          cubicInterpolationMode: 'monotone'
+        },
+        {
+          label: 'Max (\u{2103})',
+          backgroundColor: 'rgba(34, 34, 34, 1)',
+          borderColor: 'rgba(0, 0, 0, .75)',
+          borderWidth: 1,
+          data: results.map(x => Math.round(x.data.main.temp_max)),
+          fill: false,
+          cubicInterpolationMode: 'monotone'
+        },
+        {
+          label: 'Min (\u{2103})',
+          backgroundColor: 'rgb(210, 190, 42)',
+          borderColor: 'rgba(122, 122, 31, .75)',
+          borderWidth: 1,
+          data: results.map(x => Math.round(x.data.main.temp_min)),
+          fill: false,
+          cubicInterpolationMode: 'monotone'
         }
-    });
+      ]
+    }
+  });
 }, 1000);
